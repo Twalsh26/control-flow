0a46229f7fb6e89ede2a2f82663cd5cbeee5a915
Thomas <126Twaslh@frhsd.com>
2024-10-11 13:53:19 -0400

    first commit

---
+++ js/java.js
@@ -0,0 +1,16 @@
+//for loops
+
+//for(let i = 0; < 5; i++){
+
+//}
+
+//console.log('loop finished');
+
+const names = ['Shaun', 'mario', 'luigi']
+
+for(let i = 0; i < names.length++){
+    //console.log(names[i]);
+    let html = <div>${names[i]}</div>;
+    console.log(html);
+}
+
---
+++ js/while.js
@@ -0,0 +1,42 @@
+//while loops
+//const names = ['shaun', 'mario', 'lugii'];
+// let i = 0
+
+//while (1 , 5){
+//console.log('in loop: ', i);// 
+//i++;
+//}
+
+//let i = 0;
+//while(i < names.length){
+    //console.log(names[i]);
+    //i++;
+//}
+
+//do while loops
+
+//let i = 0;
+//do{
+   // console.log('val of i is: ', i);
+ //   i++;
+//} while(i < 5);
+   
+//if statements
+//const age = 25;
+
+//if (age > 20) {
+// console.log('you are over 20 years old');
+//}
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+//if (ninjas.length > 4{
+// console.log9"that's oa lot of ninjas');
+// }
+
+const password = 'p@ass';
+
+(password.length >= 8){
+    console.log('that password is long enough!');
+}
+
